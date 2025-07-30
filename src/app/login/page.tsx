77d4c909e06694e62b56605d948b08cd6abb222e
wyw0512 <87160753+wyw0512@users.noreply.github.com>
2025-07-30 08:35:01 +0800

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -33,12 +33,9 @@ function VersionDisplay() {
 
   return (
     <button
-      onClick={() =>
-        window.open('https://github.com/senshinya/MoonTV', '_blank')
-      }
       className='absolute bottom-4 left-1/2 transform -translate-x-1/2 flex items-center gap-2 text-xs text-gray-500 dark:text-gray-400 transition-colors cursor-pointer'
     >
-      <span className='font-mono'>v{CURRENT_VERSION}</span>
+      <span className='font-mono'></span>
       {!isChecking && updateStatus !== UpdateStatus.FETCH_FAILED && (
         <div
           className={`flex items-center gap-1.5 ${
