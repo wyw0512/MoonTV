f8fcb9ee3e3c5ee3be195fc32e041459ee2e4ec2
wyw0512 <87160753+wyw0512@users.noreply.github.com>
2025-07-30 10:03:31 +0800

    Update UserMenu.tsx

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -402,13 +402,13 @@ export const UserMenu: React.FC = () => {
 
           {/* 版本信息 */}
           <button
-            /* onClick={() =>
-              window.open('https://github.com/senshinya/MoonTV', '_blank')
-            } */
+            onClick={() =>
+              window.open('https://yuweitv.sisoom.com', '_blank')
+            }
             className='w-full px-3 py-2 text-center flex items-center justify-center text-gray-500 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-800/50 transition-colors text-xs'
           >
             <div className='flex items-center gap-1'>
-             /* <span className='font-mono'>v{CURRENT_VERSION}</span> */
+             <span className='font-mono'>v{CURRENT_VERSION}</span>
               {!isChecking &&
                 updateStatus &&
                 updateStatus !== UpdateStatus.FETCH_FAILED && (
